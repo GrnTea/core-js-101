69738d0d750f99e3cb00842c5828a071a5e13075
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-12-14 20:58:38 +0200

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -274,8 +274,8 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, el, index) => acc.concat(new Array(index + 1).fill(el)), []);
 }
 
 /**
